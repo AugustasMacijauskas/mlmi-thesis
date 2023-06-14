bb1b386241853ada3d81fa13d1b1fb878f991542
Augustas Macijauskas <august.macijauskas@gmail.com>
2023-06-15 00:23:01 +0100

    Finished forming datasets

--- src/dataset_formation/utils.py
+++ src/dataset_formation/utils.py
@@ -9,13 +9,34 @@ def replace_text_with_whitespace(text, new_text):
     return modified_text
 
 
+def combine_strings_with_whitespace(left_string, right_string):
+    # Calculate the length of whitespace to extract
+    whitespace_length = len(left_string) - len(left_string.lstrip())
+
+    # Extract the whitespace
+    whitespace = left_string[:whitespace_length]
+
+    # Combine the strings with the whitespace
+    combined_string = right_string + whitespace
+
+    return combined_string
+
+
 def main():
-    # Example usage
+    # Example usage of replace_text_with_whitespace
     original_text = "   Hello World!   "
     new_text = "Hi, OpenAI!"
 
     modified_text = replace_text_with_whitespace(original_text, new_text)
-    print(modified_text)  # "   Hi, OpenAI!   "
+    print(f"\"{modified_text}\"")  # "   Hi, OpenAI!   "
+
+
+    # Example usage of combine_strings_with_whitespace
+    left_string = "    Hello"
+    right_string = "World!"
+
+    combined_string = combine_strings_with_whitespace(left_string, right_string)
+    print(f"\"{combined_string}\"") # "World!    "
 
 
 if __name__ == "__main__":
